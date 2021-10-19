1455cf1eee9602c1b74e1a53cfda1d6259580b92
Mauri-CJ <mauricocella@gmail.com>
2021-10-19 11:54:50 -0300

    Migraciones De la Base de Datos

---
+++ src/app/models.py
@@ -0,0 +1,21 @@
+from .database import db
+
+class Usuario(db.Model):
+    id_usuario = db.Column(db.Integer,primary_key=True)
+    nombre = db.Column(db.String(50))
+    email = db.Column(db.String(50))
+    contraseña = db.Column(db.String(50))
+    carpetas = db.relationship('Carpeta',backref="usuario")
+
+class Carpeta(db.Model):
+    id_carpeta = db.Column(db.Integer,primary_key=True)
+    id_usuario = db.Column(db.Integer, db.ForeignKey('usuario.id_usuario'),nullable=False)
+    nombre = db.Column(db.String(50))
+    tareas = db.relationship('Tarea',backref="Carpeta")
+
+class Tarea(db.Model):
+    id_tarea = db.Column(db.Integer,primary_key=True)
+    id_carpeta = db.Column(db.Integer, db.ForeignKey('carpeta.id_carpeta'),nullable=False)
+    nombre = db.Column(db.String(50))
+    estado = db.Column(db.Boolean())
+
--- src/main.py
+++ src/main.py
@@ -1,6 +1,20 @@
 from app import create_app
+from app.database import db
+from flask_migrate import Migrate
+from app.models import Usuario,Carpeta,Tarea
+
+USER_DB='postgres'
+PASS_DB='admin'
+URL_DB='localhost'
+NAME_DB='Ensolvers'
+FULL_URL_DB=f'postgresql://{USER_DB}:{PASS_DB}@{URL_DB}/{NAME_DB}'
 
 app= create_app()
+app.config['SQLALCHEMY_DATABASE_URI']=FULL_URL_DB
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+db.init_app(app)
+migrate = Migrate()
+migrate.init_app(app,db)
 
 @app.route('/')
 def index():
