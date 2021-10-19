1a42ccf69b37da0bf9ef12430f798ee9a3d59213
Mauri-CJ <mauricocella@gmail.com>
2021-10-19 15:04:30 -0300

    Finalizando la funcionalidad del Login

--- src/app/models.py
+++ src/app/models.py
@@ -1,11 +1,15 @@
 from .database import db
+from flask_login import UserMixin
 
-class Usuario(db.Model):
+class Usuario(UserMixin,db.Model):
     id_usuario = db.Column(db.Integer,primary_key=True)
     nombre = db.Column(db.String(50))
     email = db.Column(db.String(50))
-    contraseña = db.Column(db.String(50))
+    contraseña = db.Column(db.String(250))
     carpetas = db.relationship('Carpeta',backref="usuario")
+
+    def get_id(self):
+           return (self.id_usuario)
 
 class Carpeta(db.Model):
     id_carpeta = db.Column(db.Integer,primary_key=True)
