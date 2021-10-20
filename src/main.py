9e27d445c55f5c9ae9b202c3e6f684fc48ccdf62
Mauri-CJ <mauricocella@gmail.com>
2021-10-19 21:27:29 -0300

    Agregando acciones sobre las tareas

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from flask.helpers import flash
 from jinja2.utils import contextfunction
 from app import create_app
 from app.database import db
-from app.forms import AgregarForm
+from app.forms import AgregarCarpetaForm, AgregarTareaForm,EditarTareaForm
 from flask_migrate import Migrate
 from flask_login import login_required,LoginManager,current_user,logout_user
 from app.models import Usuario,Carpeta,Tarea
@@ -34,7 +34,7 @@ def index():
     id_usuario = current_user.id_usuario
     nombre_usuario = current_user.nombre
     carpetas = Carpeta.query.filter_by(id_usuario=id_usuario)
-    agregar_form = AgregarForm()
+    agregar_form = AgregarCarpetaForm()
     context = {
         'id_usuario':id_usuario,
         'nombre_usuario': nombre_usuario,
@@ -51,13 +51,23 @@ def index():
 
     return render_template('index.html',**context)
 
-@app.route('/ver/<int:id>/<string:nombre>')
+@app.route('/ver/<int:id>/<string:nombre>',methods=['GET','POST'])
 def ver_tareas(id,nombre):
     tareas=Tarea.query.filter_by(id_carpeta=id)
+    agregar_form = AgregarTareaForm()
     context = {
         'nombre_carpeta' : nombre,
+        'id_carpeta':id,
         'tareas' : tareas,
+        'agregar_tarea' : agregar_form
     }
+
+    if request.method == 'POST':
+        if agregar_form.validate_on_submit():
+            tarea = Tarea(id_carpeta=id,nombre=agregar_form.nueva_tarea.data,estado=agregar_form.estado.data)
+            db.session.add(tarea)
+            db.session.commit()
+            return redirect(url_for('ver_tareas',id=id,nombre=nombre))
 
     return render_template('ver_tareas.html',**context)
 
@@ -71,6 +81,37 @@ def eliminar_carpeta(id):
     db.session.commit()
 
     return redirect(url_for('index'))
+
+@app.route('/eliminar_tarea/<int:id>/')
+def eliminar_tarea(id):
+    tarea =  Tarea.query.filter_by(id_tarea=id).first()
+    db.session.delete(tarea)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/editar_tarea/<int:id>', methods=['GET','POST'])
+def editar_tarea(id):
+    editar_form= EditarTareaForm()
+    tarea =  Tarea.query.filter_by(id_tarea=id).first()
+    context = {
+        'editar_form' : editar_form,
+        'tarea':tarea
+    }
+    if request.method == 'POST':
+        tarea =  Tarea.query.filter_by(id_tarea=id).first()
+        nombre = request.form['nombre']
+        estado = request.form.get('hecho')
+        if estado == 'on':
+            hecho=True
+        else:
+            hecho=False
+
+        tarea.nombre=nombre
+        tarea.estado=hecho
+        db.session.commit()
+        return redirect(url_for('index'))
+
+    return render_template('editar.html',**context)
 
 @app.route('/logout')
 @login_required
